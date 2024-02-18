bde3b52b508709547e673594888444d1a49a1c10
Rohitkumar1212 <1997errohitkumar@gmail.com>
2024-02-18 20:51:18 +0530

    added message model and conversation model

---
+++ backend/models/message.model.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    senderId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    receiverId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    message:{
+        type: String,
+        required: true,
+    }
+    //timestamp will give us createdAt and updatedAt
+},{timestamps: true})
+
+const Message = mongoose.model("Message", messageSchema)
+
+export default Message
